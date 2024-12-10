572e8a25b215761edcce5101f5ecdfda02cf9092
Jose Luis Garza <josegarzaramos@gmail.com>
2024-12-10 16:58:40 -0600

    feat: add task list change listener, event props and update task component

--- src/app/components/Task.jsx
+++ src/app/components/Task.jsx
@@ -1,24 +1,25 @@
 'use client';
-import { useState, useCallback } from 'react';
-import { LuCheck } from 'react-icons/lu';
+import { useState } from 'react';
+import { LuCheck, LuTrash } from 'react-icons/lu';
+import { useAuthContext } from '../context/AuthContext';
+import { updateTaskStatus } from '../firebase/taskService';
 
-const Task = ({ id, title, description, status }) => {
+const Task = ({ id, title, description, status, onClick, removeTask }) => {
   const [isCompleted, setIsCompleted] = useState(status === 'completed');
+  const { userId } = useAuthContext();
 
-  const handleStatusChange = useCallback(
-    (e) => {
-      const newStatus = e.target.checked ? 'completed' : 'pending';
-      setIsCompleted(e.target.checked);
-
-      // TODO: Add database functionality to update task status
-      console.log(`Task ${id} updated to ${newStatus}`);
-    },
-    [id]
-  );
+  const handleStatusChange = (e) => {
+    const newStatus = e.target.checked ? 'completed' : 'pending';
+    setIsCompleted(e.target.checked);
+    updateTaskStatus(userId, id, newStatus);
+  };
 
   return (
-    <li className="flex items-center p-6 md:p-8 border border-gray rounded">
-      <div className="flex items-center mr-4 mb-2 relative">
+    <li
+      className="flex items-center p-6 md:p-8 border border-gray rounded justify-between hover:border-blue transition-all duration-300 hover:cursor-pointer"
+      onClick={(e) => onClick(id, title, description, status, e)}
+    >
+      <div className="flex items-center mr-4 mb-2 relative gap-2 md:gap-4">
         <input
           type="checkbox"
           id={id}
@@ -35,11 +36,16 @@ const Task = ({ id, title, description, status }) => {
           htmlFor={id}
           className="select-none absolute pl-8 pr-7 md:pl-10 md:pr-7 py-10 -left-3 cursor-pointer"
         />
+        <div className="flex flex-col gap-1">
+          <h2 className="font-extrabold">{title}</h2>
+          <p>{description}</p>
+        </div>
       </div>
 
-      <div className="flex flex-col gap-1">
-        <h2 className="font-extrabold">{title}</h2>
-        <p>{description}</p>
+      <div className="hidden md:flex text-slate-400 px-2 gap-2 [&>div]:p-4">
+        <button onClick={() => removeTask(id)}>
+          <LuTrash size={'1.25em'} className="hover:text-blue" />
+        </button>
       </div>
     </li>
   );
--- src/app/components/TasksList.jsx
+++ src/app/components/TasksList.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
+import listenForTaskChanges from '../firebase/listenForTaskChanges';
 import Task from './Task';
 
-const TasksList = ({ tasks }) => {
+const TasksList = ({
+  tasks,
+  userId,
+  onClick,
+  removeTask,
+  setTasks,
+  filter,
+}) => {
+  useEffect(() => {
+    const unsubscribe = listenForTaskChanges(userId, filter, (tasks) => {
+      setTasks(tasks);
+    });
+
+    return () => unsubscribe();
+  }, [userId, filter]);
+
   const availableTasks = tasks.map(({ id, title, description, status }) => {
     return (
       <Task
@@ -9,10 +26,16 @@ const TasksList = ({ tasks }) => {
         title={title}
         description={description}
         status={status}
+        onClick={onClick}
+        removeTask={removeTask}
       />
     );
   });
 
-  return <ul className="flex flex-col gap-4 md:gap-6">{availableTasks}</ul>;
+  return (
+    <ul className="flex flex-col gap-4 md:gap-6">
+      {availableTasks.length === 0 ? 'No tasks' : availableTasks}
+    </ul>
+  );
 };
 export default TasksList;
