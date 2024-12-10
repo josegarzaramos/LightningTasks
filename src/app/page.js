a2c5d00527161319299100db2f2125c748cfea6d
Jose Luis Garza <josegarzaramos@gmail.com>
2024-12-10 17:00:52 -0600

    feat: update main page to perform CRUD operations

--- src/app/page.js
+++ src/app/page.js
@@ -1,79 +1,191 @@
 'use client';
 import { Nunito } from 'next/font/google';
+import { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthContext } from './context/AuthContext';
+import { LuX } from 'react-icons/lu';
+import { v4 as uuidv4 } from 'uuid';
+import TaskEditor from './components/TaskEditor';
 import TasksList from './components/TasksList';
 import TaskToolbar from './components/TaskToolbar';
-import NewTask from './components/NewTask';
-import { useEffect, useState } from 'react';
+
+import {
+  addTaskForUser,
+  getTasksForUser,
+  updateTaskForUser,
+  deleteTaskForUser,
+} from './firebase/taskService';
 
 const nunito = Nunito({
   weight: ['400', '800'],
   subsets: ['latin'],
 });
 
-const DUMMY_TASKS = [
-  {
-    id: 'task_0',
-    title: 'Start building todo app',
-    description: 'Start app as soon as possible',
-    status: 'completed',
-  },
-  {
-    id: 'task_1',
-    title: 'Finish todo app',
-    description: 'I need to finish this app by Thursday',
-    status: 'pending',
-  },
-  {
-    id: 'task_2',
-    title: 'Deploy finished app',
-    description: 'Deploy project and share link',
-    status: 'pending',
-  },
-  {
-    id: 'task_3',
-    title: 'Send completion email',
-    description: 'Send email when the deployed app is ready',
-    status: 'pending',
-  },
-  {
-    id: 'task_4',
-    title: 'Just a looooooooooong title',
-    description:
-      'A looooooooooooooong description for testing purposes, the purpose is to test the capability of the task component to handle looooooooooooong descriptions',
-    status: 'completed',
-  },
-];
+const defaultNewTask = {
+  id: uuidv4(),
+  title: '',
+  description: '',
+  status: 'pending',
+};
 
 export default function Home() {
-  const [tasks, setTasks] = useState(DUMMY_TASKS);
+  const [tasks, setTasks] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState('All');
+  const [taskEditor, setTaskEditor] = useState(defaultNewTask);
+  const { user, userId, loading } = useAuthContext();
+  const editorContainerRef = useRef();
+  const router = useRouter();
 
   useEffect(() => {
-    const filteredTasks = DUMMY_TASKS.filter((task) => {
-      if (selectedFilter === 'All') {
-        return true;
-      }
-      return task.status === selectedFilter.toLowerCase();
-    });
-    setTasks(filteredTasks);
+    const fetchTasks = async () => {
+      const formattedFilter = selectedFilter.toLowerCase();
+
+      const tasksData = await getTasksForUser(
+        userId,
+        formattedFilter,
+        setTasks
+      );
+      setTasks(tasksData);
+    };
+
+    if (user) {
+      fetchTasks();
+    }
+  }, [user, userId]);
+
+  useEffect(() => {
+    const fetchTasksWithFilter = async () => {
+      const formattedFilter = selectedFilter.toLowerCase();
+      const uid = user.uid;
+      const tasksData = await getTasksForUser(uid, formattedFilter, setTasks);
+      setTasks(tasksData);
+    };
+
+    if (user) {
+      fetchTasksWithFilter();
+    }
   }, [selectedFilter]);
 
   function handleFilterChange(filter) {
+    debugger;
     setSelectedFilter(filter);
   }
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push('/login');
+    }
+  }, [user, loading, router]);
+
+  if (!user) {
+    return;
+  }
+
+  const handleTaskClick = (id, title, description, status, event) => {
+    if (event.target.tagName === 'LI') {
+      setTaskEditor({
+        id,
+        title,
+        description,
+        status,
+        mode: 'edit',
+        changed: false,
+      });
+
+      if (window.innerWidth < 1024) {
+        editorContainerRef.current.classList.toggle('-translate-x-[120%]');
+        editorContainerRef.current.classList.toggle('lg:translate-x-0');
+      }
+    }
+  };
+
+  const handleRemoveTask = async (taskId) => {
+    await deleteTaskForUser(userId, taskId);
+  };
+
+  const handleAddTask = async (userId, taskId, newTask) => {
+    await addTaskForUser(userId, taskId, newTask);
+    setTaskEditor(newTask);
+
+    console.log('Setting task editor values (reset)');
+    setTaskEditor({
+      id: uuidv4(),
+      title: '',
+      description: '',
+      status: 'pending',
+      mode: 'new',
+      changed: false,
+    });
+  };
+
+  const handleCancelTaskEdit = () => {
+    setTaskEditor({
+      id: uuidv4(),
+      status: 'pending',
+      mode: 'new',
+      changed: false,
+    });
+  };
+
+  const handleSaveTask = async (taskId, updatedTask) => {
+    await updateTaskForUser(userId, taskId, updatedTask);
+
+    setTaskEditor({
+      id: uuidv4(),
+      status: 'pending',
+      mode: 'new',
+      changed: false,
+    });
+  };
 
   return (
     <main className={nunito.className}>
       <div className="grid grid-cols-1 lg:grid-cols-main h-full p-4 md:p-10 gap-4 md:gap-10 [&>div]:bg-white [&>div]:rounded [&>div]:p-4 md:[&>div]:p-10">
-        <div className="hidden lg:block">
-          <NewTask maxLength={250} />
+        <div
+          className="block absolute lg:relative w-[calc(100%-2rem)] lg:w-full z-10 lg:z-0 top-0 left-0 right-0 bottom-0 m-auto h-[calc(100%-2rem)] lg:h-full"
+          ref={editorContainerRef}
+        >
+          <div className="flex justify-end p-2 lg:hidden">
+            <button
+              onClick={() => {
+                if (window.innerWidth < 1024) {
+                  editorContainerRef.current.classList.toggle(
+                    '-translate-x-[120%]'
+                  );
+                  editorContainerRef.current.classList.toggle(
+                    'lg:translate-x-0'
+                  );
+                }
+              }}
+            >
+              <LuX className="text-blue " size={'2em'} />
+            </button>
+          </div>
+          <TaskEditor
+            maxLength={250}
+            id={taskEditor.id}
+            title={taskEditor.title}
+            description={taskEditor.description}
+            mode={taskEditor.mode}
+            changed={taskEditor.changed}
+            addTask={handleAddTask}
+            onCancel={handleCancelTaskEdit}
+            onSave={handleSaveTask}
+            userId={userId}
+          />
         </div>
         <div className="overflow-auto">
           <TaskToolbar
             onFilterChange={handleFilterChange}
             selected={selectedFilter}
           />
-          <TasksList tasks={tasks} />
+          <TasksList
+            tasks={tasks}
+            setTasks={setTasks}
+            userId={userId}
+            onClick={handleTaskClick}
+            removeTask={handleRemoveTask}
+          />
         </div>
       </div>
     </main>
