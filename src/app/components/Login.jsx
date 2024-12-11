69e309ab7e20f4661bc7c9a2113aa5d7aca6a96c
Jose Luis Garza <josegarzaramos@gmail.com>
2024-12-11 10:15:11 -0600

    feat: add login and sign up form validation

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 import { handleLogin } from '../firebase/authService';
 import { useRouter } from 'next/navigation';
 import { useAuthContext } from '../context/AuthContext';
+import { CgSpinner } from 'react-icons/cg';
 import Link from 'next/link';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const { user } = useAuthContext();
   const router = useRouter();
 
@@ -18,9 +21,17 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(e);
+    setError(false);
+    setIsLoading(true);
     const response = await handleLogin(email, password);
     if (response.status === 200) {
+      setIsLoading(false);
       router.push('/');
+    } else {
+      console.log('Wrong email/password');
+      setIsLoading(false);
+      setError('Invalid email or password.');
     }
   };
 
@@ -42,6 +53,7 @@ const Login = () => {
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -56,18 +68,32 @@ const Login = () => {
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
           <div className="text-right text-xs text-blue mr-1">
             <Link href="/reset-password">Forgot password?</Link>
           </div>
         </div>
 
-        <button
-          type="submit"
-          className="bg-blue px-10 py-4 w-auto rounded-lg font-bold text-white"
-        >
-          Login
-        </button>
+        <div className="flex flex-col justify-center">
+          <button
+            type="submit"
+            className="bg-blue px-10 py-4 w-auto rounded-lg font-bold text-white"
+          >
+            <div className="flex justify-center">
+              {loading ? (
+                <CgSpinner size={'1.50rem'} className="animate-spin mr-3" />
+              ) : (
+                'Login'
+              )}
+            </div>
+          </button>
+          {error && (
+            <span className=" text-red-600 text-sm mt-5 text-center">
+              {error}
+            </span>
+          )}
+        </div>
         <div className="flex justify-center text-zinc-600 text-sm">
           <span>Don&apos;t have account?</span>
           <Link href="/sign-up" className="text-blue ml-1">
--- src/app/components/Signup.jsx
+++ src/app/components/Signup.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { registerUser } from '../firebase/authService';
 import { useAuthContext } from '../context/AuthContext';
+import { CgSpinner } from 'react-icons/cg';
 import Link from 'next/link';
 
 const SignUp = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const { user } = useAuthContext();
   const router = useRouter();
 
@@ -19,9 +22,15 @@ const SignUp = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const response = await registerUser({ email, password, name });
     if (response) {
+      setIsLoading(false);
+      setError('');
       router.push('/');
+    } else {
+      setError('There was an error, try again.');
+      setIsLoading(false);
     }
   };
 
@@ -43,6 +52,7 @@ const SignUp = () => {
             placeholder="Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -57,6 +67,7 @@ const SignUp = () => {
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -71,18 +82,32 @@ const SignUp = () => {
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
           <div className="text-right text-xs text-blue mr-1">
             <Link href="/reset-password">Forgot password?</Link>
           </div>
         </div>
 
-        <button
-          type="submit"
-          className="bg-blue px-10 py-4 w-auto rounded-lg font-bold text-white"
-        >
-          Sign Up
-        </button>
+        <div className="flex flex-col justify-center">
+          <button
+            type="submit"
+            className="bg-blue px-10 py-4 w-auto rounded-lg font-bold text-white"
+          >
+            <div className="flex justify-center">
+              {loading ? (
+                <CgSpinner size={'1.50rem'} className="animate-spin mr-3" />
+              ) : (
+                'Sign up'
+              )}
+            </div>
+          </button>
+          {error && (
+            <span className=" text-red-600 text-sm mt-5 text-center">
+              {error}
+            </span>
+          )}
+        </div>
         <div className="flex justify-center text-zinc-600 text-sm">
           <span>Already have account?</span>
           <Link href="/login" className="text-blue ml-1">
