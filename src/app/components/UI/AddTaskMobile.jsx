33aabbba9dbe81a125e2b3e04c08de59ff63267e
Jose Luis Garza <josegarzaramos@gmail.com>
2024-12-11 22:08:09 -0600

    style: update add task mobile styles

--- src/app/components/UI/AddTaskMobile.jsx
+++ src/app/components/UI/AddTaskMobile.jsx
@@ -3,10 +3,10 @@ import { PiNotePencilLight } from 'react-icons/pi';
 const AddTaskMobile = ({ onClick }) => {
   return (
     <div
-      className="absolute bottom-24 right-8 bg-blue p-4 rounded-lg text-white lg:hidden"
+      className="absolute bottom-1/3 right-4 md:right-10 bg-blue p-4 rounded-[18px_4px_4px_18px] text-white lg:hidden"
       onClick={onClick}
     >
-      <PiNotePencilLight size={'2.2rem'} />
+      <PiNotePencilLight size={'2.2rem'} className="animate-pulse" />
     </div>
   );
 };
